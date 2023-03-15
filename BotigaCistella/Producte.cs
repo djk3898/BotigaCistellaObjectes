6163e7c98f5909f180a739a00048f2617cd307b7
djk3898 <92979584+djk3898@users.noreply.github.com>
2023-03-15 12:10:27 +0100

    Constructors i Metodes classe Producte

--- BotigaCistella/Producte.cs
+++ BotigaCistella/Producte.cs
@@ -11,7 +11,7 @@ namespace BotigaCistella
         //Atributs
         private string nom;
         private double preuSenseIva;
-        private int iva;
+        private double iva;
         private int quantitat;
         //Propietats
         public string Nom
@@ -24,7 +24,7 @@ namespace BotigaCistella
             get { return preuSenseIva; }
             set { if (value > 0) preuSenseIva = value; }
         }
-        public int Iva
+        public double Iva
         {
             get { return iva; }
             set { if (value > 0) iva = value; }
@@ -35,8 +35,31 @@ namespace BotigaCistella
             set { if(value > 0) quantitat = value; }
         }
         //Constructors
-
+        public Producte()
+        {
+            iva = 1.21;
+            quantitat = 0;
+        }
+        public Producte(string nom, int preu) : this()
+        {
+            this.nom = nom;
+            preuSenseIva = preu;
+        }
+        public Producte(string nom, int preu, int iva, int quantitat)
+        {
+            this.nom = nom;
+            preuSenseIva = preu;
+            this.iva = iva;
+            this.quantitat = quantitat;
+        }
         //Metodes
-
+        public double Preu()
+        {
+            return (preuSenseIva * iva) * quantitat;
+        }
+        public string ToString()
+        {
+            return $"Nom: {nom}\nPreu: {Preu()}";
+        }
     }
 }
